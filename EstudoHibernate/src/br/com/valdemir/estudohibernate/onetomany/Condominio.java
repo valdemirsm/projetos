d37a4bd7bd1ffde746b7c962715a8f52a6e404f3
valdemirsm <demirsantos@hotmail.com>
2015-09-03 22:32:58 -0400

    asdfas

---
+++ EstudoHibernate/src/br/com/valdemir/estudohibernate/onetomany/Condominio.java
@@ -0,0 +1,82 @@
+package br.com.valdemir.estudohibernate.onetomany;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity(name="condominio")
+public class Condominio implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id", nullable=false)
+	private Integer id;
+	@Column(name="nome")
+	private String nome;
+	
+	@OneToMany(mappedBy = "condominio")
+	private Collection<CondominioCasa> condominioCasas;
+	
+	public Condominio() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Collection<CondominioCasa> getCondominioCasas() {
+		return condominioCasas;
+	}
+
+	public void setCondominioCasas(Collection<CondominioCasa> condominioCasas) {
+		this.condominioCasas = condominioCasas;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Condominio other = (Condominio) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+
+
+}
---
+++ EstudoHibernate/src/br/com/valdemir/estudohibernate/onetomany/CondominioCasa.java
@@ -0,0 +1,76 @@
+package br.com.valdemir.estudohibernate.onetomany;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+//@NamedQuery(name="Pessoa.lista", query="SELECT new br.com.valdemir.estudohibernate.query.ResumoPessoa(p.id, p.nome) FROM Pessoa p")
+@Entity(name = "condominioCasa")
+public class CondominioCasa implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Integer id;
+	@Column(name = "nome", nullable = false)
+	private String nome;
+	
+	@ManyToOne
+	@JoinColumn(name="idCondominio")
+	private Condominio condominio;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	public Condominio getCondominio() {
+		return condominio;
+	}
+	public void setCondominio(Condominio condominio) {
+		this.condominio = condominio;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CondominioCasa other = (CondominioCasa) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+
+
+}
